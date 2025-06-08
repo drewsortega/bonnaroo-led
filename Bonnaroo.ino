873cd75f480baa5425ed850a60392dc73793e176
Drew Ortega <orvyx@Drews-MacBook-Pro.local>
2025-06-08 16:28:56 -0700

    Initial commit - working IR screen and turn on/off SD. Probably need to use different SD library next.

---
+++ Bonnaroo.ino
@@ -0,0 +1,390 @@
+/*
+ * This SmartMatrix Library example displays GIF animations loaded from a SD Card connected to the Teensy 3/4 and ESP32
+ *
+ * This example requires SmartMatrix Library 4.0 and AnimatedGIF Library to be installed, you can do this from Arduino Library Manager
+ *   - https://github.com/pixelmatix/SmartMatrix
+ *   - https://github.com/bitbank2/AnimatedGIF
+ *
+ * Wiring is on the default Teensy 3.2 SPI pins, and chip select can be on any GPIO,
+ * set by defining SD_CS in the code below.  For Teensy 3.5/3.6/4.1 with the onboard SDIO, SD_CS should be the default BUILTIN_SDCARD
+ * Function     | Pin
+ * DOUT         |  11
+ * DIN          |  12
+ * CLK          |  13
+ * CS (default) |  15
+ *
+ * Wiring for ESP32 follows the default for the ESP32 SD Library, see: https://github.com/espressif/arduino-esp32/tree/master/libraries/SD
+ *
+ * This code first looks for .gif files in the /gifs/ directory
+ * (customize below with the GIF_DIRECTORY definition) then plays random GIFs in the directory,
+ * looping each GIF for DISPLAY_TIME_SECONDS
+ *
+ * This example is meant to give you an idea of how to add GIF playback to your own sketch.
+ * For a project that adds GIF playback with other features, take a look at
+ * Light Appliance and Aurora:
+ * https://github.com/CraigLindley/LightAppliance
+ * https://github.com/pixelmatix/aurora
+ *
+ * If you find any GIFs that won't play properly, please attach them to a new
+ * Issue post in the GitHub repo here:
+ * https://github.com/pixelmatix/AnimatedGIFs/issues
+ */
+
+/*
+ * CONFIGURATION:
+ *  - Set the chip select pin for your board.  On Teensy 3.5/3.6/4.1, the onboard microSD CS pin is "BUILTIN_SDCARD"
+ *  - For ESP32 used with large panels, you don't need to lower the refreshRate, but you can lower the frameRate (number of times the refresh buffer
+ *    is updaed with new data per second), giving more time for the CPU to decode the GIF.
+ *    Use matrix.setMaxCalculationCpuPercentage() or matrix.setCalcRefreshRateDivider()
+ */
+
+#include <GifDecoder.h>
+#include <IRremote.hpp>
+#include <MatrixHardware_Teensy4_ShieldV5.h>        // SmartLED Shield for Teensy 4 (V5)
+#include <SD.h>
+#include <SmartMatrix.h>
+
+#include "FilenameFunctions.h"
+
+#define DISPLAY_TIME_SECONDS 10
+#define NUMBER_FULL_CYCLES   100
+
+// Teensy 4.0 using CS1
+#define SD_CS 0
+// If use_sd = False, then colors will be used instead for testing.
+const bool use_sd = false;
+// Teensy 4.1 with builtin
+// #define SD_CS BUILTIN_SDCARD
+
+// Teensy SD Library requires a trailing slash in the directory name
+#define GIF_DIRECTORY "/gifs/"
+
+// Data pin the IR receiver is hooked up to.
+#define IR_RECEIVE_PIN 16
+
+// range 0-255
+const int defaultBrightness = 255;
+
+const rgb24 COLOR_BLACK = {
+    0, 0, 0 };
+const rgb24 COLOR_WHITE = {
+    255, 0, 0 };
+const rgb24 COLOR_GREEN = {
+    0, 255, 0 };
+const rgb24 COLOR_BLUE = {
+    0, 0, 255 };
+const rgb24 COLOR_RED = {
+    255, 0, 255 };
+
+/* SmartMatrix configuration and memory allocation */
+#define COLOR_DEPTH 24                  // Choose the color depth used for storing pixels in the layers: 24 or 48 (24 is good for most sketches - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24)
+const uint16_t kMatrixWidth = 64;       // Set to the width of your display, must be a multiple of 8
+const uint16_t kMatrixHeight = 64;      // Set to the height of your display
+const uint8_t kRefreshDepth = 36;       // Tradeoff of color quality vs refresh rate, max brightness, and RAM usage.  36 is typically good, drop down to 24 if you need to.  On Teensy, multiples of 3, up to 48: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48.  On ESP32: 24, 36, 48
+const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save RAM, more to keep from dropping frames and automatically lowering refresh rate.  (This isn't used on ESP32, leave as default)
+const uint8_t kPanelType = SM_PANELTYPE_HUB75_32ROW_MOD16SCAN;  // Choose the configuration that matches your panels.  See more details in MatrixCommonHub75.h and the docs: https://github.com/pixelmatix/SmartMatrix/wiki
+const uint32_t kMatrixOptions = (SMARTMATRIX_OPTIONS_C_SHAPE_STACKING);        // see docs for options: https://github.com/pixelmatix/SmartMatrix/wiki
+const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
+const uint8_t kScrollingLayerOptions = (SM_SCROLLING_OPTIONS_NONE);
+
+SMARTMATRIX_ALLOCATE_BUFFERS(matrix, kMatrixWidth, kMatrixHeight, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
+SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kBackgroundLayerOptions);
+SMARTMATRIX_ALLOCATE_SCROLLING_LAYER(scrollingLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kScrollingLayerOptions);
+
+/* template parameters are maxGifWidth, maxGifHeight, lzwMaxBits
+ * 
+ * lzwMaxBits is included for backwards compatibility reasons, but isn't used anymore
+ */
+GifDecoder<kMatrixWidth, kMatrixHeight, 12> decoder;
+
+int num_files;
+
+void screenClearCallback(void) {
+  backgroundLayer.fillScreen({0,0,0});
+}
+
+void updateScreenCallback(void) {
+  backgroundLayer.swapBuffers();
+}
+
+void drawPixelCallback(int16_t x, int16_t y, uint8_t red, uint8_t green, uint8_t blue) {
+    backgroundLayer.drawPixel(x, y, {red, green, blue});
+}
+
+int wrap_initFileSystem(int chipSelectPin) {
+    if (use_sd) {
+        return initFileSystem(chipSelectPin);
+    }
+    return 0;
+}
+
+int wrap_enumerateGIFFiles(const char *directoryName, bool displayFilenames) {
+    if (use_sd) {
+        return wrap_enumerateGIFFiles(directoryName, displayFilenames);
+    }
+    return 3;
+}
+
+// Remote Layout:
+//
+// BUT_VOL_DOWN     BUT_PLAY        BUT_VOL_UP
+// BUT_SETUP        BUT_UP          BUT_STOP
+// BUT_LEFT         BUT_ENTER       BUT_RIGHT
+// BUT_0            BUT_DOWN        BUT_BACK
+// BUT_1            BUT_2           BUT_3
+// BUT_4            BUT_5           BUT_6
+// BUT_7            BUT_8           BUT_9
+
+
+#define BUT_VOL_DOWN    0xFF00BF00
+#define BUT_PLAY        0xFE01BF00
+#define BUT_VOL_UP      0xFD02BF00
+#define BUT_SETUP       0xFB04BF00
+#define BUT_UP          0xFA05BF00
+#define BUT_STOP        0xF906BF00
+#define BUT_LEFT        0xF708BF00
+#define BUT_ENTER       0xF609BF00
+#define BUT_RIGHT       0xF50ABF00
+#define BUT_0           0xF30CBF00
+#define BUT_DOWN        0xF20DBF00
+#define BUT_BACK        0xF10EBF00
+#define BUT_1           0xEF10BF00
+#define BUT_2           0xEE11BF00
+#define BUT_3           0xED12BF00
+#define BUT_4           0xEB14BF00
+#define BUT_5           0xEA15BF00
+#define BUT_6           0xE916BF00
+#define BUT_7           0xE718BF00
+#define BUT_8           0xE619BF00
+#define BUT_9           0xE51ABF00
+
+bool buttonToStr(IRRawDataType button, char* buf) {
+    switch(button) {
+        case BUT_VOL_DOWN:
+            strcat(buf, "VOL_DOWN");
+            break;
+        case BUT_PLAY:
+            strcat(buf, "PLAY");
+            break;
+        case BUT_VOL_UP:
+            strcat(buf, "VOL_UP");
+            break;
+        case BUT_SETUP:
+            strcat(buf, "SETUP");
+            break;
+        case BUT_UP:
+            strcat(buf, "UP");
+            break;
+        case BUT_STOP:
+            strcat(buf, "STOP");
+            break;
+        case BUT_LEFT:
+            strcat(buf, "LEFT");
+            break;
+        case BUT_ENTER:
+            strcat(buf, "ENTER");
+            break;
+        case BUT_RIGHT:
+            strcat(buf, "RIGHT");
+            break;
+        case BUT_0:
+            strcat(buf, "0");
+            break;
+        case BUT_DOWN:
+            strcat(buf, "DOWN");
+            break;
+        case BUT_BACK:
+            strcat(buf, "BACK");
+            break;
+        case BUT_1:
+            strcat(buf, "1");
+            break;
+        case BUT_2:
+            strcat(buf, "2");
+            break;
+        case BUT_3:
+            strcat(buf, "3");
+            break;
+        case BUT_4:
+            strcat(buf, "4");
+            break;
+        case BUT_5:
+            strcat(buf, "5");
+            break;
+        case BUT_6:
+            strcat(buf, "6");
+            break;
+        case BUT_7:
+            strcat(buf, "7");
+            break;
+        case BUT_8:
+            strcat(buf, "8");
+            break;
+        case BUT_9:
+            strcat(buf, "9");
+            break;
+        default:
+            strcat(buf, "Unknown Button!");
+            return false;
+    }
+    return true;
+}
+
+
+// Setup method runs once, when the sketch starts
+void setup() {
+    // ----------------------------------------------
+    // ---------- GIF Decorder Setup ----------------
+    // ----------------------------------------------
+    decoder.setScreenClearCallback(screenClearCallback);
+    decoder.setUpdateScreenCallback(updateScreenCallback);
+    decoder.setDrawPixelCallback(drawPixelCallback);
+
+    decoder.setFileSeekCallback(fileSeekCallback);
+    decoder.setFilePositionCallback(filePositionCallback);
+    decoder.setFileReadCallback(fileReadCallback);
+    decoder.setFileReadBlockCallback(fileReadBlockCallback);
+    
+    // NOTE: new callback function required after we moved to using the external AnimatedGIF library to decode GIFs
+    decoder.setFileSizeCallback(fileSizeCallback);
+
+    Serial.begin(115200);
+
+    // give time for USB Serial to be ready
+    delay(1000);
+
+    Serial.println("Starting AnimatedGIFs Sketch");
+
+    matrix.addLayer(&backgroundLayer); 
+    matrix.addLayer(&scrollingLayer);
+
+    matrix.setBrightness(defaultBrightness);
+
+
+    // for large panels, may want to set the refresh rate lower to leave more CPU time to decoding GIFs (needed if GIFs are playing back slowly)
+    //matrix.setRefreshRate(90);
+    matrix.begin();
+
+
+    // Clear screen
+    backgroundLayer.fillScreen(COLOR_BLACK);
+    backgroundLayer.swapBuffers();
+    scrollingLayer.setMode(stopped);
+    scrollingLayer.setColor({0xff, 0xff, 0xff});
+
+
+    // ----------------------------------------------
+    // ---------- SD Card Setup  --------------------
+    // ----------------------------------------------
+    if(wrap_initFileSystem(SD_CS) < 0) {
+        scrollingLayer.start("No SD card", -1);
+        Serial.println("No SD card");
+        while(1);
+    } else if (use_sd) {
+        scrollingLayer.start("SD Card found!", -1);
+    }
+    if (use_sd) {
+        while(1);
+    }
+
+    // Determine how many animated GIF files exist
+    num_files = wrap_enumerateGIFFiles(GIF_DIRECTORY, true);
+
+    if(num_files < 0) {
+        scrollingLayer.start("No gifs directory", -1);
+        Serial.println("No gifs directory");
+        while(1);
+    }
+
+    if(!num_files) {
+        scrollingLayer.start("Empty gifs directory", -1);
+        Serial.println("Empty gifs directory");
+        while(1);
+    }
+
+  IrReceiver.begin(IR_RECEIVE_PIN, DISABLE_LED_FEEDBACK); // Start the receiver
+}
+
+
+void loop() {
+    static unsigned long displayStartTime_millis;
+    static int nextGIF = 1;     // we haven't loaded a GIF yet on first pass through, make sure we do that
+
+    unsigned long now = millis();
+
+    static int index = 0;
+
+#if 1
+    // default behavior is to play the gif for DISPLAY_TIME_SECONDS or for NUMBER_FULL_CYCLES, whichever comes first
+    if((now - displayStartTime_millis) > (DISPLAY_TIME_SECONDS * 1000)/* || (use_sd && decoder.getCycleNumber() > NUMBER_FULL_CYCLES)*/)
+        nextGIF = 1;
+#else
+    // alt behavior is to play the gif until both DISPLAY_TIME_SECONDS and NUMBER_FULL_CYCLES have passed
+    if((now - displayStartTime_millis) > (DISPLAY_TIME_SECONDS * 1000) /*&& (use_sd && decoder.getCycleNumber() > NUMBER_FULL_CYCLES)*/)
+        nextGIF = 1;
+#endif
+
+    char str_buf[300];
+    str_buf[0] = 0;
+    scrollingLayer.setFont(font5x7);
+    if (IrReceiver.decode()) {
+        IRRawDataType received_data = IrReceiver.decodedIRData.decodedRawData;
+        // Noisy data that fails to interpret is received as 0. Filter out the
+        // erroneous results.
+        if (received_data != 0){
+            strcat(str_buf, "IR: ");
+            if (!buttonToStr(received_data, str_buf)) {
+                strcat(str_buf, String(received_data, HEX).c_str());
+            }
+            scrollingLayer.start(str_buf, -1);
+        }
+        IrReceiver.resume(); // Receive the next value
+    }
+    // else if(nextGIF)  {
+    //     nextGIF = 0;
+
+    //     if (use_sd) {
+    //         if (openGifFilenameByIndex(GIF_DIRECTORY, index) >= 0) {
+    //             // Can clear screen for new animation here, but this might cause flicker with short animations
+    //             // matrix.fillScreen(COLOR_BLACK);
+    //             // matrix.swapBuffers();
+
+    //             // start decoding, skipping to the next GIF if there's an error
+    //             if(decoder.startDecoding() < 0) {
+    //                 nextGIF = 1;
+    //                 return;
+    //             }
+
+    //         }
+    //     } else {
+    //         switch(index) {
+    //             case 0:
+    //                 strcat(str_buf, "Zero");
+    //                 break;
+    //             case 1:
+    //                 strcat(str_buf, "One");
+    //                 break;
+    //             case 2:
+    //                 strcat(str_buf, "Two");
+    //                 break;
+    //             default:
+    //                 strcat(str_buf, "Oops!");
+    //         }
+
+    //         scrollingLayer.start(str_buf, -1);
+    //         // Calculate time in the future to terminate animation
+    //         displayStartTime_millis = now;
+    //     }
+
+    //     // get the index for the next pass through
+    //     if (++index >= num_files) {
+    //         index = 0;
+    //     }
+
+    // }
+
+    if(use_sd && decoder.decodeFrame() < 0) {
+        // There's an error with this GIF, go to the next one
+        nextGIF = 1;
+    }
+}
